0434c4b1c511df8f2c0e59dd6fe897f9e3324917
vboxuser <johnarmstrong117@gmail.com>
2024-02-13 14:36:25 -0800

    Adding find_command method

--- lab1_Armstrong.c
+++ lab1_Armstrong.c
@@ -38,3 +38,15 @@ int main() {
 	}
 }
 
+int find_command(char* user_command)
+{
+    int i = 0;
+    while(cmd[i]){
+        if (strcmp(user_command, cmd[i])==0){
+            return i;
+        }
+        i++;
+    }
+    return -1;
+}
+
